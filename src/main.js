b74495d2281365db9daaec0021f8375eb588c61b
JoshSevy <joshuasevy@outlook.com>
2020-12-09 02:36:31 -0700

    Refactored functions to be singular purpose

--- src/main.js
+++ src/main.js
@@ -23,19 +23,27 @@ let homeView = document.querySelector('.home-view');
 let formView = document.querySelector('.form-view');
 let savedView = document.querySelector('.saved-view');
 
+// Form Inputs
+let userCover = document.querySelector('.user-cover');
+let userTitle = document.querySelector('.user-title');
+let userDesc1 = document.querySelector('.user-desc1');
+let uersDesc2 = document.querySelector('.user-desc2');
+
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 
-getRandomCover();
 
-var currentCover;
+var currentCover = newRandomCover();
+
+displayCurrentCover()
 
 // Add your event listeners here 👇
 
 randomCoverBtn.addEventListener('click',function() {
-  getRandomCover();
+  currentCover = newRandomCover();
+  displayCurrentCover();
 });
 
 btnControls.addEventListener('click', function(event) {
@@ -46,9 +54,7 @@ btnControls.addEventListener('click', function(event) {
   if (event.target.className === 'save-cover-button') {
     saveMainCover()
   }
-  
-})
-
+});
 
 // Create your event handlers and other functions here 👇
 
@@ -56,53 +62,59 @@ function changeView(event) {
   if (event.target.className === 'make-new-button') {
     homeView.classList.add('hidden');
     formView.classList.remove('hidden');
+    savedView.classList.add('hidden');
+
     makeCoverBtn.classList.add('hidden');
     homeBtn.classList.remove('hidden');
     saveBtn.classList.add('hidden');
-    savedView.classList.add('hidden');
     randomCoverBtn.classList.add('hidden');
   }
 
   if (event.target.className === 'home-button') {
     homeView.classList.remove('hidden');
     formView.classList.add('hidden');
+    savedView.classList.add('hidden');
+
     makeCoverBtn.classList.remove('hidden');
     homeBtn.classList.add('hidden');
     saveBtn.classList.remove('hidden');
-    savedView.classList.add('hidden');
     randomCoverBtn.classList.remove('hidden');
   }
 
   if (event.target.className === 'view-saved-button') {
     homeView.classList.add('hidden');
-    homeBtn.classList.remove('hidden');
-    savedView.classList.remove('hidden');
-    saveBtn.classList.add('hidden');
     formView.classList.add('hidden');
+    savedView.classList.remove('hidden');
+
+    homeBtn.classList.remove('hidden');
+    saveBtn.classList.add('hidden');
     randomCoverBtn.classList.add('hidden');
-    getSavedCovers(savedCovers);
+
+    displaySavedCovers(savedCovers);
   }
-  
-}
+};
 
+function displayCurrentCover() {
+  mainCoverImage.src = currentCover.cover;
+  mainCoverTitle.innerHTML = currentCover.title;
+  mainCoverTag1.innerHTML = currentCover.tagline1;
+  mainCoverTag2.innerHTML = currentCover.tagline2;
+};
 
-function getRandomCover() {
-  var randomCover = new Cover(
+function newRandomCover() {
+  return new Cover(
   covers[getRandomIndex(covers)], 
   titles[getRandomIndex(titles)], 
   descriptors[getRandomIndex(descriptors)],
   descriptors[getRandomIndex(descriptors)]
   ); 
-
-  currentCover = randomCover;
-
-  mainCoverImage.src = currentCover.cover;
-  mainCoverTitle.innerHTML = currentCover.title;
-  mainCoverTag1.innerHTML = currentCover.tagline1;
-  mainCoverTag2.innerHTML = currentCover.tagline2;
 }
 
-function getSavedCovers() {
+function saveMainCover() {
+  savedCovers.push(currentCover);
+};
+
+function displaySavedCovers() {
   savedCoversSection.innerHTML = null;
   for (let i = 0; i < savedCovers.length; i++) {
     savedCoversSection.innerHTML +=
@@ -112,13 +124,16 @@ function getSavedCovers() {
         <h3 class="tagline">A tale of <span class="tagline-1">${savedCovers[i].tagline1}</span> and <span class="tagline-2">${savedCovers[i].tagline2}</span></h3>
       </section>`
   }
-}
+};
 
-function saveMainCover() {
-  savedCovers.push(currentCover);
+function userCreatedBook() {
+  cover.push(userCover.value)
+  title.push(userTitle.value);
+  descriptors.push(userDesc1.value);
+  descriptors.push(userDesc2.value);
 }
 
 // We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
-}
+};
